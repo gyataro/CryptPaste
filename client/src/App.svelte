63ca048e941bf8ca0a3cf93253ec1537ce2e02b6
gyataro <teoh6g@gmail.com>
2022-03-07 16:16:50 +0800

    Added TailwindCSS

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,9 +1,10 @@
 <script>
+	import Style from './Style.svelte';
 	export let name;
 </script>
 
 <main>
-	<h1>Hello, {name}!</h1>
+	<h1 class="py-8 px-4 font-extrabold border border-indigo-900 shadow-lg">Hello, {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
